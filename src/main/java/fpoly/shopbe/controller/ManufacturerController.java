68862bd302c4a1c455d629a3ce32849c92730388
kmink <kiennt0498@gmail.com>
2023-07-22 04:10:08 +0700

    create manufacturer

--- src/main/java/fpoly/shopbe/controller/ManufacturerController.java
+++ src/main/java/fpoly/shopbe/controller/ManufacturerController.java
@@ -2,16 +2,20 @@ package fpoly.shopbe.controller;
 
 import fpoly.shopbe.DTO.ManufacturerDto;
 import fpoly.shopbe.domain.Manufacturer;
+import fpoly.shopbe.exception.FileStorageException;
 import fpoly.shopbe.service.FileStorageService;
 import fpoly.shopbe.service.ManufacturerService;
 import fpoly.shopbe.service.MapValidationErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
@@ -43,5 +47,24 @@ public class ManufacturerController {
         dto.setLogo(entity.getLogo());
 
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
+    }
+    @GetMapping("logo/{filename:.+}")
+    public ResponseEntity dowloadFile(@PathVariable String filename, HttpServletRequest req){
+        Resource resource = file.loadLogoFileAsResource(filename);
+
+        String contentType = null;
+        try {
+            contentType = req.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        }catch (Exception e){
+            throw new FileStorageException("Could not determine file type.");
+        }
+
+        if(contentType == null){
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename\""
+                + resource.getFilename()+"\"").body(resource);
     }
 }
